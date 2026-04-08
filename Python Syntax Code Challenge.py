dff65bf8f88efbad57122e56aa6bae8a177148d1
Deep Patole <deeppatole99@gmail.com>
2026-04-08 21:19:12 +0530

    challenge1

---
+++ Python Syntax Code Challenge.py
@@ -0,0 +1,11 @@
+# Inside the editor, complete the following steps:
+# Write a statement that prints "Hello World!"
+# Write a statement that prints "Have a good day."
+# Write a statement that prints "Learning Python is fun!"
+
+# Print "Hello World!"
+print("Hello World!")
+# Print "Have a good day."
+print("Have a good day.")
+# Print "Learning Python is fun!"
+print("Learning Python is fun!")
